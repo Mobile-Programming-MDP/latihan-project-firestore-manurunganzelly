c08a87a39793bed9335849a077d43812bd4038c7
manurunganzelly <161702416+manurunganzelly@users.noreply.github.com>
2024-05-20 17:08:18 +0700

    flutter maps widget

--- notes/lib/screens/note_list_screen.dart
+++ notes/lib/screens/note_list_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:notes/models/note.dart';
+import 'package:notes/screens/maps_screen.dart';
 import 'package:notes/services/note_service.dart';
 import 'package:notes/widgets/note_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
@@ -51,27 +55,28 @@ class NoteList extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           default:
-           return ListView(
+            return ListView(
               padding: const EdgeInsets.only(bottom: 80),
               children: snapshot.data!.map((document) {
                 return Card(
                   child: Column(
                     children: [
-                      document.imageUrl != null && Uri.parse(document.imageUrl!).isAbsolute
-                      ? ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(16),
-                          topRight: Radius.circular(16),
-                        ),
-                        child: Image.network(
-                          document.imageUrl!,
-                          width: double.infinity,
-                          height: 150,
-                          fit: BoxFit.cover,
-                          alignment: Alignment.center,
-                        ),
-                      )
-                      : Container(),
+                      document.imageUrl != null &&
+                              Uri.parse(document.imageUrl!).isAbsolute
+                          ? ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(16),
+                                topRight: Radius.circular(16),
+                              ),
+                              child: Image.network(
+                                document.imageUrl!,
+                                width: double.infinity,
+                                height: 150,
+                                fit: BoxFit.cover,
+                                alignment: Alignment.center,
+                              ),
+                            )
+                          : Container(),
                       ListTile(
                         onTap: () {
                           showDialog(
@@ -83,14 +88,46 @@ class NoteList extends StatelessWidget {
                         },
                         title: Text(document.title),
                         subtitle: Text(document.description),
-                        trailing: InkWell(
-                          onTap: () {
-                            showAlertDialog(context, document);
-                          },
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 10),
-                            child: Icon(Icons.delete),
-                          ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            document.lat != null && document.lng != null
+                                ? InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => MapScreen(
+                                            document.lat,
+                                            document.lng,
+                                          ),
+                                        ),
+                                      );
+                                      //openMap(document.lat, document.lng);
+                                    },
+                                    child: const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      child: Icon(Icons.map),
+                                    ),
+                                  )
+                                : const Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 10),
+                                    child: Icon(
+                                      Icons.map,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                            InkWell(
+                              onTap: () {
+                                showAlertDialog(context, document);
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(Icons.delete),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -101,6 +138,14 @@ class NoteList extends StatelessWidget {
         }
       },
     );
+  }
+
+  Future<void> openMap(String? lat, String? lng) async {
+    Uri uri =
+        Uri.parse("https://www.google.com/maps/search/?api=1&query=$lat, $lng");
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch $uri');
+    }
   }
 
   showAlertDialog(BuildContext context, Note document) {
